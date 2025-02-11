37977044086de1cdff333eb8a78e73f330b35208
itayalasas <info@ayalait.com.uy>
2025-02-11 01:40:48 -0300

    update cambios entidad ventas

--- terminal-web/src/main/java/com/ayalait/terminal/modelo/Ventas.java
+++ terminal-web/src/main/java/com/ayalait/terminal/modelo/Ventas.java
@@ -1,169 +1,227 @@
 package com.ayalait.terminal.modelo;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 import javax.persistence.*;
 
 @Entity
 @NamedStoredProcedureQueries({
-    @NamedStoredProcedureQuery(
-        name = "actualizarTransaccionTarjetaVenta",
-        procedureName = "actualizarTransaccionTarjetaVenta",
-        parameters = {
-          @StoredProcedureParameter(mode=ParameterMode.IN, name="idVenta", type=Integer.class),
-          @StoredProcedureParameter(mode=ParameterMode.IN, name="id_transaccion", type=String.class)
-          
-    })
-})
-@Table(name="ventas")
+		@NamedStoredProcedureQuery(name = "actualizarTransaccionTarjetaVenta", procedureName = "actualizarTransaccionTarjetaVenta", parameters = {
+				@StoredProcedureParameter(mode = ParameterMode.IN, name = "idVenta", type = Integer.class),
+				@StoredProcedureParameter(mode = ParameterMode.IN, name = "id_transaccion", type = String.class)
+
+		}) })
+@Table(name = "ventas")
 public class Ventas implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-     @Id
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id_venta; 
-	private int estado; 
-	private String id_usuario;
-	private String fecha_hora; 
-	private String fecha_hora_cerrado; 
-	private String id_usuario_autoriza;
-	private int id_sucursal; 
-	private String fecha_baja; 
-	private int movimiento; 
-	private double monto_total; 
-	private String id_cliente; 
-	private int id_apertura_cajero;
-	private int nro_consecutivo; 
-	private double iva5; 
-	private double iva10; 
-	private double iva22;
-	private int condicion; 
-	private double monto_credito; 
-	private double redondeo;
-	private String id_transaccion;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_venta")
+	private int idVenta;
+	private int estado;
+	@Column(name = "id_usuario")
+	private String idUsuario;
+	@Column(name = "fecha_hora")
+	private String fechaHora;
+	@Column(name = "fecha_hora_cerrado")
+	private String fechaHoraCerrado;
 
-	
-	public int getId_venta() {
-		return id_venta;
+	@Column(name = "id_usuario_autoriza")
+	private String idUsuarioAutoriza;
+
+	@Column(name = "id_sucursal")
+	private int idSucursal;
+
+	@Column(name = "fecha_baja")
+	private String fechaBaja;
+
+	private int movimiento;
+
+	@Column(name = "monto_total")
+	private double montoTotal;
+
+	@Column(name = "id_cliente")
+	private String idCliente;
+
+	@Column(name = "id_apertura_cajero")
+	private int idAperturaCajero;
+
+	@Column(name = "nro_consecutivo")
+	private int nroConsecutivo;
+	private double iva5;
+	private double iva10;
+	private double iva22;
+	private int condicion;
+
+	@Column(name = "monto_credito")
+	private double montoCredito;
+	private double redondeo;
+
+	@Column(name = "id_transaccion")
+	private String idTransaccion;
+
+	public int getIdVenta() {
+		return idVenta;
 	}
-	public void setId_venta(int id_venta) {
-		this.id_venta = id_venta;
+
+	public void setIdVenta(int idVenta) {
+		this.idVenta = idVenta;
 	}
+
 	public int getEstado() {
 		return estado;
 	}
+
 	public void setEstado(int estado) {
 		this.estado = estado;
 	}
-	public String getId_usuario() {
-		return id_usuario;
+
+	public String getIdUsuario() {
+		return idUsuario;
 	}
-	public void setId_usuario(String id_usuario) {
-		this.id_usuario = id_usuario;
+
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
 	}
-	public String getFecha_hora() {
-		return fecha_hora;
+
+	public String getFechaHora() {
+		return fechaHora;
 	}
-	public void setFecha_hora(String fecha_hora) {
-		this.fecha_hora = fecha_hora;
+
+	public void setFechaHora(String fechaHora) {
+		this.fechaHora = fechaHora;
 	}
-	public String getFecha_hora_cerrado() {
-		return fecha_hora_cerrado;
+
+	public String getFechaHoraCerrado() {
+		return fechaHoraCerrado;
 	}
-	public void setFecha_hora_cerrado(String fecha_hora_cerrado) {
-		this.fecha_hora_cerrado = fecha_hora_cerrado;
+
+	public void setFechaHoraCerrado(String fechaHoraCerrado) {
+		this.fechaHoraCerrado = fechaHoraCerrado;
 	}
-	public String getId_usuario_autoriza() {
-		return id_usuario_autoriza;
+
+	public String getIdUsuarioAutoriza() {
+		return idUsuarioAutoriza;
 	}
-	public void setId_usuario_autoriza(String id_usuario_autoriza) {
-		this.id_usuario_autoriza = id_usuario_autoriza;
+
+	public void setIdUsuarioAutoriza(String idUsuarioAutoriza) {
+		this.idUsuarioAutoriza = idUsuarioAutoriza;
 	}
-	public int getId_sucursal() {
-		return id_sucursal;
+
+	public int getIdSucursal() {
+		return idSucursal;
 	}
-	public void setId_sucursal(int id_sucursal) {
-		this.id_sucursal = id_sucursal;
+
+	public void setIdSucursal(int idSucursal) {
+		this.idSucursal = idSucursal;
 	}
-	public String getFecha_baja() {
-		return fecha_baja;
+
+	public String getFechaBaja() {
+		return fechaBaja;
 	}
-	public void setFecha_baja(String fecha_baja) {
-		this.fecha_baja = fecha_baja;
+
+	public void setFechaBaja(String fechaBaja) {
+		this.fechaBaja = fechaBaja;
 	}
+
 	public int getMovimiento() {
 		return movimiento;
 	}
+
 	public void setMovimiento(int movimiento) {
 		this.movimiento = movimiento;
 	}
-	public double getMonto_total() {
-		return monto_total;
+
+	public double getMontoTotal() {
+		return montoTotal;
 	}
-	public void setMonto_total(double monto_total) {
-		this.monto_total = monto_total;
+
+	public void setMontoTotal(double montoTotal) {
+		this.montoTotal = montoTotal;
 	}
-	public String getId_cliente() {
-		return id_cliente;
+
+	public String getIdCliente() {
+		return idCliente;
 	}
-	public void setId_cliente(String id_cliente) {
-		this.id_cliente = id_cliente;
+
+	public void setIdCliente(String idCliente) {
+		this.idCliente = idCliente;
 	}
-	public int getId_apertura_cajero() {
-		return id_apertura_cajero;
+
+	public int getIdAperturaCajero() {
+		return idAperturaCajero;
 	}
-	public void setId_apertura_cajero(int id_apertura_cajero) {
-		this.id_apertura_cajero = id_apertura_cajero;
+
+	public void setIdAperturaCajero(int idAperturaCajero) {
+		this.idAperturaCajero = idAperturaCajero;
 	}
-	public int getNro_consecutivo() {
-		return nro_consecutivo;
+
+	public int getNroConsecutivo() {
+		return nroConsecutivo;
 	}
-	public void setNro_consecutivo(int nro_consecutivo) {
-		this.nro_consecutivo = nro_consecutivo;
+
+	public void setNroConsecutivo(int nroConsecutivo) {
+		this.nroConsecutivo = nroConsecutivo;
 	}
+
 	public double getIva5() {
 		return iva5;
 	}
+
 	public void setIva5(double iva5) {
 		this.iva5 = iva5;
 	}
+
 	public double getIva10() {
 		return iva10;
 	}
+
 	public void setIva10(double iva10) {
 		this.iva10 = iva10;
 	}
-	public int getCondicion() {
-		return condicion;
-	}
-	public void setCondicion(int condicion) {
-		this.condicion = condicion;
-	}
-	public double getMonto_credito() {
-		return monto_credito;
-	}
-	public void setMonto_credito(double monto_credito) {
-		this.monto_credito = monto_credito;
-	}
-	public double getRedondeo() {
-		return redondeo;
-	}
-	public void setRedondeo(double redondeo) {
-		this.redondeo = redondeo;
-	}
-	public String getId_transaccion() {
-		return id_transaccion;
-	}
-	public void setId_transaccion(String id_transaccion) {
-		this.id_transaccion = id_transaccion;
-	}
+
 	public double getIva22() {
 		return iva22;
 	}
+
 	public void setIva22(double iva22) {
 		this.iva22 = iva22;
 	}
-	 
+
+	public int getCondicion() {
+		return condicion;
+	}
+
+	public void setCondicion(int condicion) {
+		this.condicion = condicion;
+	}
+
+	public double getMontoCredito() {
+		return montoCredito;
+	}
+
+	public void setMontoCredito(double montoCredito) {
+		this.montoCredito = montoCredito;
+	}
+
+	public double getRedondeo() {
+		return redondeo;
+	}
+
+	public void setRedondeo(double redondeo) {
+		this.redondeo = redondeo;
+	}
+
+	public String getIdTransaccion() {
+		return idTransaccion;
+	}
+
+	public void setIdTransaccion(String idTransaccion) {
+		this.idTransaccion = idTransaccion;
+	}
 	
 	
+
 }
